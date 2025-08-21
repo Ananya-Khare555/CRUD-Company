6fc380c62df43709bb1e8db3d9ac8b348f072787
Ananya-Khare555 <ananyakhare555@gmail.com>
2025-08-21 12:07:35 +0530

    Updating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import { Routes } from '@angular/router';
 import { StartPageComponent } from './features/start-page/start-page.component';
-
-// Sidebar layout + feature pages
 import { LayoutComponent } from './layout/layout.component'; 
 import { CompanyListComponent } from './features/company-list/company-list.component';
 import { NewCompanyComponent } from './features/new-company/new-company.component';
 
 export const routes: Routes = [
-  // 👇 Default splash page (no sidebar)
+  
   { path: '', component: StartPageComponent },
 
-  // 👇 CRUD area (with sidebar)
+  
   {
     path: '',
     component: LayoutComponent,
@@ -20,6 +18,5 @@ export const routes: Routes = [
     ]
   },
 
-  // Fallback
   { path: '**', redirectTo: '' }
 ];
